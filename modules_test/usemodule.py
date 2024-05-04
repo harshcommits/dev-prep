7802174a802a3c2dcca33a1489ec9b73ae3ce390
Harsh Jha <jhaharsh96@gmail.com>
2024-05-04 09:47:19 +0530

    prelim test files

---
+++ modules_test/usemodule.py
@@ -0,0 +1,3 @@
+from primes import listPrimes, isPrime
+
+print(isPrime(6))
---
+++ primes.py
@@ -0,0 +1,25 @@
+def check_for_prime(number):
+
+    flag = False
+
+    if number == 1:
+        print(number, "is not a prime numberber")
+    elif number > 1:
+        # check for factors
+        for i in range(2, int(number/2) + 1):
+            if (number % i) == 0:
+                # if factor is found, set flag to True
+                flag = True
+                # break out of loop
+                break
+
+        # check if flag is True
+        if flag:
+            print(number, "is not a prime number")
+        else:
+            print(number, "is a prime number")
+
+
+if __name__ == "__main__":
+
+    check_for_prime(10)
