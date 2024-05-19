2a21170425fbe9e3549dbb437642f9aa6a896d3a
Harsh Jha <jhaharsh96@gmail.com>
2024-05-19 23:39:52 +0530

    updated functions and stuff

--- variable_scope.py
+++ variable_scope.py
@@ -5,6 +5,7 @@ the following code snippets detail out the variable scopes in different scenario
 # check for local variables; returns a dictionary of all variables as key-value pairs
 def localCheck(num1, num2, operation='sum'):
     print(locals())
+    # pass
 
 def globalCheck(num1, num2, operation='multiply'):
     print(globals())
@@ -12,3 +13,5 @@ def globalCheck(num1, num2, operation='multiply'):
 
 if __name__ == "__main__":
     localCheck(1, 3, 'multiply')
+    # globalCheck(1, 2)
+    print('breaker')
