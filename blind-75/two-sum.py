30f4de5ec6f96bd93af43b5124da96718c213c75
Harsh Jha <jhaharsh96@gmail.com>
2024-05-26 18:46:40 +0530

    added code for two sum

--- blind-75/two-sum.py
+++ blind-75/two-sum.py
@@ -1,8 +1,14 @@
+# brute force method; efficiency is O(n2)
 def two_sum(values, sum):
-    pass
+    for i in range (0, len(values)-1):
+        for j in range(i+1, len(values)-1):
+            if values[i] +  values[j] == sum:
+                return f"The respective numbers are {values[i]} and {values[j]}"
+
+    return "The values don't exist"
 
 
 if __name__ == "__main__":
     
     values = [1, 2, 4, 5, 67]
-    two_sum(values, 9)
+    print(two_sum(values, 10))
