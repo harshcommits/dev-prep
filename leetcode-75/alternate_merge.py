edce7937682e9c22b70090b1a7558223716b58b7
Harsh Jha <jhaharsh96@gmail.com>
2024-06-09 06:53:59 +0530

    added to leetcode-75

--- leetcode-75/alternate_merge.py
+++ leetcode-75/alternate_merge.py
@@ -1,5 +1,20 @@
-def alternate_merge(a, b):
-    pass
+def alternate_merge(word1, word2):
+    i = j = 0
+    result = ""
+
+    max_index = max(len(word1), len(word2))
+    for i in range(0, max_index):
+        if i < len(word1) and i < len(word2):
+            result += word1[i] + word2[i]
+
+        if i <= len(word1) and i >= len(word2):
+            result += word1[i]
+
+        if i >= len(word1) and i <= len(word2):
+            result += word2[i]
+
+    return result
 
 if __name__ == "__main__":
-    alternate_merge("ab", "cd")
+    values = alternate_merge("abc", "de")
+    print(values)
---
+++ leetcode-75/gcd-of-strings.py
@@ -0,0 +1,24 @@
+def gcdOfStrings(str1, str2):
+    len1, len2 = len(str1), len(str2)
+
+    def valid(k):
+        if len1 % k or len2 % k:
+            return False
+        
+        n1, n2 = len1 // k, len2 // k
+        base = str1[:k]
+
+        ## debug stuff
+        print(base)
+        print(str1 == n1 * base and str2 == n2 * base)
+        return str1 == n1 * base and str2 == n2 * base
+
+    for i in range(min(len1, len2), 0, -1):
+        if valid(i):
+            return str1[:i] 
+
+
+
+if __name__ == "__main__":
+    # print(gcdOfStrings("ABABAB", "AB"))
+    print(gcdOfStrings("ABABAB", "CODE"))
